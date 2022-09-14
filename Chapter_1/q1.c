8743777da4cb073826d2e5776ecd0be8287c6445
Chitransh Mall <chitranshmall6827@gmail.com>
2022-09-14 20:12:12 +0530

    perimeter of rectangle sides a,b taken as an input from the user

--- Chapter_1/q1.c
+++ Chapter_1/q1.c
@@ -6,6 +6,6 @@ int main()
     printf("Enter the values of a & b: ");
     scanf("%d %d",&a,&b);
     //perimeter of rectangle = 2*(a+b)
-    printf("Perimeter of rectangle is: %d",2*(a+b));    
+    printf("Perimeter of rectangle is- %d",2*(a+b));    
     return 0;
 }
