2f392f798735cdd9dbfaa17e5856c3edf83ef636
Chitransh Mall <chitranshmall6827@gmail.com>
2022-09-14 22:24:17 +0530

    check if character is digit or not

--- Chapter_2/q2.c
+++ Chapter_2/q2.c
@@ -6,6 +6,6 @@ int main()
     printf("Enter the character: ");
     scanf("%c",&ch);
 //ternary operator is to run one code when the condition is true and another code when the condition is false (one line code for checking true or false condition)
-    ch>='0' && ch <='9' ? printf("Its is a digit") : printf("Its is not a digit");
+    ch>='0' && ch <='9' ? printf("Its is a digit!") : printf("Its is not a digit!");
     return 0;
 }
