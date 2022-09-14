6426297fc51109a840566e6a14d317b177b6e0d6
Chitransh Mall <chitranshmall6827@gmail.com>
2022-09-14 22:22:49 +0530

    print the avg. of 3 numbers inputed by user

--- Chapter_2/q1.c
+++ Chapter_2/q1.c
@@ -5,6 +5,6 @@ int main()
     int a,b,c;
     printf("Enter the three numbers(a b c): ");
     scanf("%d %d %d",&a,&b,&c);
-    printf("Average of three numbers is: %d", (a+b+c)/3);
+    printf("Average of three numbers is- %d", (a+b+c)/3);
     return 0;
 }
