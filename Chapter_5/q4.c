9ec501b8a8d1cb6edcf39542a9d86d804a827c73
Chitransh Mall <chitranshmall6827@gmail.com>
2022-09-14 19:54:14 +0530

    make your own pow function

--- Chapter_5/q4.c
+++ Chapter_5/q4.c
@@ -1,1 +1,20 @@
 //Make your own pow function.
+#include<stdio.h>
+void powr(int x, int y)
+{
+    int p = x;
+    for(int i=1;i<y;i++)
+    {
+        p=p*x;
+    }
+    printf("Ans: %d",p);
+}
+
+int main()
+{
+    int x,y;
+    printf("Enter the number & its base power(n1 bp): ");
+    scanf("%d %d",&x,&y);
+    powr(x,y);
+    return 0;
+}
