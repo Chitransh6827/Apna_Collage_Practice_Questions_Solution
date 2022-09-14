ce2e94cc46fd404d5558127beae0300e3b700840
Chitransh Mall <chitranshmall6827@gmail.com>
2022-09-14 22:27:38 +0530

    check if no. is natural no. or not

--- Chapter_3/q2.c
+++ Chapter_3/q2.c
@@ -4,6 +4,6 @@ int main(){
     int n;
     printf("Enter the number: ");
     scanf("%d",&n);
-    n>=1 ? printf("Number Entered is a NATURAL NUMBER '%d' ",n) : printf("Enterd Number is NOT A NATURAL NUMBER '%d' ",n);
+    n>=1 ? printf("Number entered is a NATURAL NUMBER '%d' ",n) : printf("Enterd Number is NOT A NATURAL NUMBER '%d' ",n);
     return 0;
 }
